f5823fa2d1108ee82f22cdfcc132515f07b1f661
Razak <moabdulrazak17@gmail.com>
2022-08-29 23:21:20 +0000

    style(global): create global wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components';
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+
+const Container = styled.div`
+	display: flex;
+`;
+
+const Main = styled.div`
+	flex: 7;
+`;
+const Wrapper = styled.div`
+	padding: 22px 96px;
+`;
+
 function App() {
-	return <div className='App'>Hello Mom!</div>;
+	return (
+		<Container>
+			<Menu />
+			<Main>
+				<Navbar />
+				<Wrapper>Video Cards</Wrapper>
+			</Main>
+		</Container>
+	);
 }
 
 export default App;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+	flex: 1;
+  background-color: #030303;
+  color: white;
+	height: 100vh;
+`;
+
+const Menu = () => {
+	return <Container> Menu </Container>;
+};
+
+export default Menu;
