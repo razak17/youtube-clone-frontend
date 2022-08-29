8c8b05701b4d73f9466714ed5bc95f3843ef6fd4
Razak <moabdulrazak17@gmail.com>
2022-08-29 23:28:46 +0000

    chore(styles): css reset

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,13 +2,20 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 	flex: 1;
-  background-color: #030303;
-  color: white;
+	background-color: #030303;
+	color: white;
 	height: 100vh;
+`;
+const Wrapper = styled.div`
+	padding: 18px 26px;
 `;
 
 const Menu = () => {
-	return <Container> Menu </Container>;
+	return (
+		<Container>
+			<Wrapper>Menu</Wrapper>
+		</Container>
+	);
 };
 
 export default Menu;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
+
+/* eslint-disable-next-line no-unused-vars */
+export type FC<T> = (val: T) => JSX.Element;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
