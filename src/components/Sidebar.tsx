6b13c35261da89ca5b54a4d5cf70951e5c7c2bd3
Razak <moabdulrazak17@gmail.com>
2022-08-30 00:44:33 +0000

    chore: format

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import LamaTube from "../assets/logo.png";
+import LamaTube from '../assets/logo.png';
 
 const Container = styled.div`
 	flex: 1;
@@ -13,33 +13,32 @@ const Wrapper = styled.div`
 	padding: 18px 26px;
 `;
 const Logo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  margin-bottom: 20px;
+	display: flex;
+	align-items: center;
+	gap: 5px;
+	margin-bottom: 20px;
 
-  span {
-    font-weight: bold;
-    font-size: 18px;
-
-  }
+	span {
+		font-weight: bold;
+		font-size: 18px;
+	}
 `;
 
 const Img = styled.img`
-  height: 25px;
+	height: 25px;
 `;
 
 const Sidebar = () => {
 	return (
 		<Container>
 			<Wrapper>
-        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-          <Logo>
-            <Img src={LamaTube} />
-            <span>DevTube</span>
-          </Logo>
-        </Link>
-        </Wrapper>
+				<Link to='/' style={{ textDecoration: 'none', color: 'inherit' }}>
+					<Logo>
+						<Img src={LamaTube} />
+						<span>DevTube</span>
+					</Logo>
+				</Link>
+			</Wrapper>
 		</Container>
 	);
 };
