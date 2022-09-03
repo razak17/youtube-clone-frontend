888a62c044befb82257c6ebe2c0aeda45f70636f
Razak <moabdulrazak17@gmail.com>
2022-09-03 19:03:18 +0000

    chore(styles): move css reset and font to html
    remove index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 /* eslint-disable-next-line no-unused-vars */
 export type FC<T> = (val: T) => JSX.Element;
