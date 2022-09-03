efd0070f068db59bef5afa3b355a514560df608f
Razak <moabdulrazak17@gmail.com>
2022-09-03 19:32:27 +0000

    feat(pages): setup Sign In page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,93 +1,88 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: calc(100vh - 56px);
-  color: ${({ theme }) => theme.text};
+const StyledContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: calc(100vh - 56px);
+	color: ${({ theme }) => theme.text};
+	padding-top: 48px;
 `;
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  background-color: ${({ theme }) => theme.bgLighter};
-  border: 1px solid ${({ theme }) => theme.soft};
-  padding: 20px 50px;
-  gap: 10px;
+const StyledSection = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	width: 250px;
+	border-radius: 3px;
+	background-color: ${({ theme }) => theme.bgLighter};
+	border: 1px solid ${({ theme }) => theme.soft};
+	padding: 20px 50px;
+	gap: 10px;
 `;
 
-const Title = styled.h1`
-  font-size: 24px;
+const StyledTitle = styled.div`
+	margin-bottom: 18px;
+	h1 {
+		font-size: 24px;
+	}
 `;
 
-const SubTitle = styled.h2`
-  font-size: 20px;
-  font-weight: 300;
+const StyledSubTitle = styled.div`
+	padding: 32px 0;
+	p {
+		font-size: 14px;
+		font-weight: 300;
+	}
 `;
 
-const Input = styled.input`
-  border: 1px solid ${({ theme }) => theme.soft};
-  border-radius: 3px;
-  padding: 10px;
-  background-color: transparent;
-  width: 100%;
-  color: ${({ theme }) => theme.text};
+const StyledInput = styled.input`
+	border: 1px solid ${({ theme }) => theme.soft};
+	border-radius: 3px;
+	padding: 10px;
+	background-color: transparent;
+	width: 100%;
+	outline: none;
+	color: ${({ theme }) => theme.text};
 `;
 
-const Button = styled.button`
-  border-radius: 3px;
-  border: none;
-  padding: 10px 20px;
-  font-weight: 500;
-  cursor: pointer;
-  background-color: ${({ theme }) => theme.soft};
-  color: ${({ theme }) => theme.textSoft};
-`;
-
-const More = styled.div`
-  display: flex;
-  margin-top: 10px;
-  font-size: 12px;
-  color: ${({ theme }) => theme.textSoft};
-`;
-
-const Links = styled.div`
-  margin-left: 50px;
-`;
-
-const Link = styled.span`
-  margin-left: 30px;
+const StyledButton = styled.button`
+	border-radius: 3px;
+	border: none;
+	padding: 10px 24px;
+	font-weight: 500;
+	font-size: 16px;
+	cursor: pointer;
+	background-color: ${({ theme }) => theme.soft};
+	color: ${({ theme }) => theme.textSoft};
 `;
 
 const SignIn = () => {
-  return (
-    <Container>
-      <Wrapper>
-        <Title>Sign in</Title>
-        <SubTitle>to continue to LamaTube</SubTitle>
-        <Input placeholder="username" />
-        <Input type="password" placeholder="password" />
-        <Button>Sign in</Button>
-        <Title>or</Title>
-        <Input placeholder="username" />
-        <Input placeholder="email" />
-        <Input type="password" placeholder="password" />
-        <Button>Sign up</Button>
-      </Wrapper>
-      <More>
-        English(USA)
-        <Links>
-          <Link>Help</Link>
-          <Link>Privacy</Link>
-          <Link>Terms</Link>
-        </Links>
-      </More>
-    </Container>
-  );
+	return (
+		<StyledContainer>
+			<StyledSection>
+				<StyledTitle>
+					<h1>Sign In</h1>
+				</StyledTitle>
+				<StyledInput placeholder='Username' />
+				<StyledInput type='password' placeholder='Password' />
+				<StyledButton>Sign In</StyledButton>
+			</StyledSection>
+			<StyledSubTitle>
+				<p>New User?,</p>
+			</StyledSubTitle>
+			<StyledSection>
+				<StyledTitle>
+					<h1>Sign Up</h1>
+				</StyledTitle>
+				<StyledInput placeholder='Username' />
+				<StyledInput placeholder='Email' />
+				<StyledInput type='password' placeholder='Password' />
+				<StyledButton>Sign Up</StyledButton>
+			</StyledSection>
+		</StyledContainer>
+	);
 };
 
 export default SignIn;
-
