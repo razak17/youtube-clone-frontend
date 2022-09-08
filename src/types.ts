99244629c6b60a245d84ccc4812a63a355601f33
Razak <moabdulrazak17@gmail.com>
2022-09-08 20:52:01 +0000

    feat(context): create videos context

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+export interface Video {
+  _id: string;
+  owner: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  description: string;
+  title: string;
+}
+export interface Comment {
+  _id: string;
+  owner: string;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  videoId: string;
+  title: string;
+}
+
+export enum QueryKeys {
+  me = "me",
+  videos = "videos",
+}
+
+export interface Me {
+  _id: string;
+  email: string;
+  username: string;
+}
+
